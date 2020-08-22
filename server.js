52060f772a8dfaf09482cef03e470a82da5b3f20
Diogo Silverio <38838233+dblsilverio@users.noreply.github.com>
2020-08-22 14:29:15 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ function sleep(ms) {
 async function main() {
   while(true) {
     console.log('Containers rule!');
+    console.log(`Place ${process.env.FAVORITE_PLACE}`);
     await sleep(5000);
   }
 }
